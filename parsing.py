23733718719047f7846a297f787143d5d7bd64d5
flaviobeck <flaviobeck@yahoo.com>
2020-05-26 23:27:20 -0300

    parsing to output file

--- parsing.py
+++ parsing.py
@@ -1,37 +1,70 @@
+#!/usr/bin/python
 import xml.etree.ElementTree as ET
-
 
 # Load XML file
 doc = ET.parse('target/mutations.xml')
-mutants = doc.getroot() # Recovers main tag 
+mutations = doc.getroot() # Recovers main tag
+
+# Prepare output XML
+root = ET.Element('mutations')
 
 # Parsing XML elements
+for mutation in mutations:
 
-for mutant in mutants:
-    if mutant.attrib['status'] == 'SURVIVED':
-        #print mutant.tag, mutant.attrib['detected'],  mutant.attrib['status'], mutant.attrib['numberOfTestsRun']
-        numberOfTestsRun = mutant.attrib['numberOfTestsRun']
-        sourceFile = mutant.findtext('sourceFile')
-        mutatedClass = mutant.findtext('mutatedClass') 
-        mutatedMethod = mutant.findtext('mutatedMethod')
-        methodDescription = mutant.findtext('methodDescription')
-        lineNumber = mutant.findtext('lineNumber')
-        mutator = mutant.findtext('mutator') 
-        index = mutant.findtext('index')
-        block = mutant.findtext('block')
-        killingTests = mutant.findtext('killingTests')
-        succeedingTests = mutant.findtext('succeedingTests')
-        description = mutant.findtext('description')
-        
-        #print succeedingTests
-        print numberOfTestsRun, sourceFile
+    detected = mutation.attrib['detected']
+    status = mutation.attrib['status']
+    numberOfTestsRun = mutation.attrib['numberOfTestsRun']
+    sourceFile = mutation.findtext('sourceFile')
+    mutatedClass = mutation.findtext('mutatedClass') 
+    mutatedMethod = mutation.findtext('mutatedMethod')
+    methodDescription = mutation.findtext('methodDescription')
+    lineNumber = mutation.findtext('lineNumber')
+    mutator = mutation.findtext('mutator') 
+    index = mutation.findtext('index')
+    block = mutation.findtext('block')
+    killingTests = mutation.findtext('killingTests')
+    succeedingTests = mutation.findtext('succeedingTests')
+    description = mutation.findtext('description')
+    
+    if status == 'SURVIVED':
+
+        # Create an element for each succeeding
         testList = succeedingTests.split("|")
+
         for test in testList:
-            print test
+            # Add elements into new XML root
+            _mutation = ET.SubElement(root, 'mutation', detected=detected, status=status, numberOfTestsRun=numberOfTestsRun)
+            ET.SubElement(_mutation, "sourceFile").text = sourceFile
+            ET.SubElement(_mutation, "mutatedClass").text = mutatedClass
+            ET.SubElement(_mutation, "mutatedMethod").text = mutatedMethod
+            ET.SubElement(_mutation, "methodDescription").text = methodDescription
+            ET.SubElement(_mutation, "lineNumber").text = lineNumber
+            ET.SubElement(_mutation, "mutator").text = mutator
+            ET.SubElement(_mutation, "index").text = index
+            ET.SubElement(_mutation, "block").text = block
+            ET.SubElement(_mutation, "killingTests").text = killingTests
+            ET.SubElement(_mutation, "succeedingTests").text = test
+            ET.SubElement(_mutation, "description").text = description
 
+    else:
+        # Create an element for each killing
+        testList = killingTests.split("|")
 
+        for test in testList:
+            # Add elements into new XML root
+            _mutation = ET.SubElement(root, 'mutation', detected=detected, status=status, numberOfTestsRun=numberOfTestsRun)
+            ET.SubElement(_mutation, "sourceFile").text = sourceFile
+            ET.SubElement(_mutation, "mutatedClass").text = mutatedClass
+            ET.SubElement(_mutation, "mutatedMethod").text = mutatedMethod
+            ET.SubElement(_mutation, "methodDescription").text = methodDescription
+            ET.SubElement(_mutation, "lineNumber").text = lineNumber
+            ET.SubElement(_mutation, "mutator").text = mutator
+            ET.SubElement(_mutation, "index").text = index
+            ET.SubElement(_mutation, "block").text = block
+            ET.SubElement(_mutation, "killingTests").text = test
+            ET.SubElement(_mutation, "succeedingTests").text = succeedingTests
+            ET.SubElement(_mutation, "description").text = description
 
-        
-    
-
-    
+# Write tree as new file
+tree = ET.ElementTree(root)
+tree.write('target/mutation_parsed.xml') 
